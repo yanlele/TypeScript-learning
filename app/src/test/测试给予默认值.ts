6498d4a922a033a3f5c0a3d124d4fae0d54dfb1d
yanlele <331393627@qq.com>
2018-10-03 19:27:22 +0800

    [+]提交一个解构赋值的例子

---
+++ app/src/test/测试给予默认值.ts
@@ -0,0 +1,17 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2018-10-03 19:25
+ */
+
+
+
+let {name = '', age ='111'} = {
+    name: 'yanle'
+};
+
+console.log(name);
+console.log(age);
+
+
+export default {};
