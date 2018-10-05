0d206072dfc9b30b36559508435766925d353c51
yanlele <331393627@qq.com>
2018-10-05 23:32:30 +0800

    [+]关于泛型的研究

--- app/src/01、基础部分/06、泛型/index.ts
+++ app/src/01、基础部分/06、泛型/index.ts
@@ -3,3 +3,23 @@ function loggingIdentity<T>(arg: T): T {
     console.log(arg.length);  // Error: T doesn't have .length
     return arg;
 }*/
+
+
+interface User {
+    name: string
+}
+
+class Test {
+    getName() {
+        this.checkAnswer<User>()
+    }
+
+
+    checkAnswer<User>() {
+        let data: User;
+        data = {
+            name: 'yanle'
+        };
+        return data
+    }
+}
